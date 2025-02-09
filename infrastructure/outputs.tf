dd37c1c9e94359080dda0eb97d158024e1f26b99
Nir Gluzman <nir.gluzman@gmail.com>
2025-02-09 21:34:28 +0100

    feat: add Cognito User Pool ID and Client ID to outputs

--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -2,5 +2,15 @@
 
 output "apigw_url" {
   description = "URL for API Gateway"
-  value = aws_apigatewayv2_api.apigw_http.api_endpoint
+  value = module.apigw_park.api_endpoint
+}
+
+output "cognito_user_pool_id" {
+  description = "Cognito User Pool ID"
+  value = aws_cognito_user_pool.main.id
+}
+
+output "cognito_user_pool_client_id" {
+  description = "Client ID associated with the Cognito User Pool"
+  value = aws_cognito_user_pool_client.main.id
 }
