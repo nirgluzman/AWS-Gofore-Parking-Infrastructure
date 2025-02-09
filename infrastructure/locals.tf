ae05986a18ed437b4412f2fa0aaec532db299c0a
Nir Gluzman <nir.gluzman@gmail.com>
2025-02-09 21:38:07 +0100

    refactor: move all variables to locals.tf, keep only aws_region as an external input to TF config

--- infrastructure/locals.tf
+++ infrastructure/locals.tf
@@ -1,10 +1,21 @@
 locals {
   app_name          = "park-manager"
 
+  user_pool_name          = "${local.app_name}-admin-users"   # Cognito user pool name
+  user_pool_client_name   = "${local.app_name}-admin-client"  # Cognito user pool client name (friendly identifier)
+  user_pool_domain_prefix = "${local.app_name}"               # Cognito domain prefix (Cognito's hosted UI)
+  user_pool_client_callback_urls =  [                         # Cognito user pool client callback URLs
+      "http://localhost:3000/callback"    # local development
+  ]
+
   apigw_name        = "${local.app_name}-api"
   apigw_stage_name  = "dev"
 
   lambda_get_data_admin_name = "${local.app_name}-getDataAdmin"
+  lambda_get_data_user_name  = "${local.app_name}-getDataUser"
+  lambda_put_data_user_name  = "${local.app_name}-putDataUser"
 
   ddb_table_name    = "${local.app_name}-cars-data"
+
+  log_retention     = 7 # log retention in days
 }
