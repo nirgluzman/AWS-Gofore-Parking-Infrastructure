6f5215eefce1e049e54d0b409f55958e461f909c
Nir Gluzman <nir.gluzman@gmail.com>
2025-02-09 21:32:45 +0100

    feat: update password_policy

--- infrastructure/cognito_user_pool.tf
+++ infrastructure/cognito_user_pool.tf
@@ -1,7 +1,9 @@
+#-------------------------------------------------------------------
 # Cognito User Pool
+#-------------------------------------------------------------------
 
 resource "aws_cognito_user_pool" "main" {
-  name                     = var.user_pool_name
+  name                     = local.user_pool_name
   auto_verified_attributes = ["email"] # the user attributes (email or phone_number) we want Cognito to auto send the confirmation code or link
 
   alias_attributes         = ["email"] # allows users to log in using attributes other than their username
@@ -10,12 +12,13 @@ resource "aws_cognito_user_pool" "main" {
     email_sending_account = "COGNITO_DEFAULT" # Email delivery method to use
   }
 
+  # Password must be at least 8 characters long, contains at least one number, one lowercase and one uppercase.
   password_policy {
-    minimum_length    = 6 # minimum length of the password
-    require_lowercase = false # whether you've required users to use at least one lowercase letter in their password
+    minimum_length    = 8     # minimum length of the password
+    require_lowercase = true  # whether you've required users to use at least one lowercase letter in their password
     require_numbers   = true  # whether you've required users to use at least one number in their password
     require_symbols   = false
-    require_uppercase = false
+    require_uppercase = true
   }
 
   mfa_configuration = "OFF" # MFA Tokens are not required
@@ -42,11 +45,11 @@ resource "aws_cognito_user_pool" "main" {
 
 resource "aws_cognito_user_pool_client" "main" {
   user_pool_id          = aws_cognito_user_pool.main.id # user pool the client belongs to
-  name                  = var.user_pool_client_name # name of the application client (friendly identifier)
+  name                  = local.user_pool_client_name # name of the application client (friendly identifier)
   read_attributes       = ["email"] # defines the actual user profile fields that are readable
   write_attributes      = ["email"] # defines the user profile fields that are writable
 
-  callback_urls         = var.user_pool_client_callback_urls # URLs in application that Cognito redirects users to after successful authentication
+  callback_urls         = local.user_pool_client_callback_urls # URLs in application that Cognito redirects users to after successful authentication
   allowed_oauth_flows   = ["implicit"] # access tokens are returned directly in the redirect URL - can be a security risk !!
   allowed_oauth_flows_user_pool_client = true
   allowed_oauth_scopes  = ["openid", "profile"] # controls what data and operations the client application can access through OAuth flows
@@ -65,6 +68,6 @@ resource "aws_cognito_user_pool_client" "main" {
 
 # configure a domain for Cognito's hosted UI, https://<domain>.auth.<AWS region code>.amazoncognito.com
 resource "aws_cognito_user_pool_domain" "main" {
-  domain = var.user_pool_domain_prefix # the domain prefix
+  domain = local.user_pool_domain_prefix # the domain prefix
   user_pool_id = aws_cognito_user_pool.main.id
 }
