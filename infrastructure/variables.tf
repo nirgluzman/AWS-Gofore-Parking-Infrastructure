ec39078c4898ebb95e6484f4e197e440869d69f2
Nir Gluzman <nir.gluzman@gmail.com>
2025-02-09 21:37:29 +0100

    refactor: move all variables to locals.tf, keep only aws_region as an external input to TF config

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -5,35 +5,3 @@ variable "aws_region" {
   type    = string
   default = "us-east-1"
 }
-
-variable "user_pool_name" {
-  description = "Cognito user pool name"
-  type        = string
-  default     = "park-manager-admin-users"
-}
-
-variable "user_pool_client_name" {
-  description = "Cognito user pool client name (friendly identifier)"
-  type        = string
-  default     = "park-manager-admin-client"
-}
-
-variable "user_pool_client_callback_urls" {
-  description = "Cognito user pool client callback URLs"
-  type        = list(string)
-  default     = [
-      "http://localhost:3000/callback"    # local development
-  ]
-}
-
-variable "user_pool_domain_prefix" {
-  description = "Cognito domain prefix (Cognito's hosted UI)"
-  type        = string
-  default     = "park-manager"
-}
-
-variable "log_retention" {
-  description = "log retention in days"
-  type = number
-  default = 7
-}
